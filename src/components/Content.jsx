c1b4979cc2a91d1c9447f16c837a6853584e211d
IdemudiaMahie <idemudiamahie23@gmail.com>
2026-02-06 00:00:52 -0500

    Home.jsx
    Home.jsx was worked on.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Projects from '../pages/projects'
+import Services from '../pages/Services'
+import Contact from '../pages/Contact'
 
 function Content() {
   return (
-    <div>Content</div>
+    <main>
+        <Home />
+        <About />
+        <Projects />
+        <Services />
+        <Contact />
+    </main>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <>
+      <h1>Welcome to My Portfolio</h1>
+      <p>
+        Hello! I'm Osamahiemen Idemudia, a Full Stack MERN Developer with a
+        passion for crafting clean, responsive, and user-focused web
+        applications. My mission is to leverage my creativity and technical
+        expertise to build impactful digital experiences that solve real-world
+        problems.
+      </p>
+      <a href="#">Learn More About Me</a>
+    </>
+  );
+}
+
+export default Home;

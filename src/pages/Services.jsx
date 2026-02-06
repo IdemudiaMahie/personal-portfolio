dc4243080ea8fb224d49288fec0ef711785c6466
IdemudiaMahie <idemudiamahie23@gmail.com>
2026-02-06 12:26:13 -0500

    Updated Services.jsx
    Pasted code from HTML

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,9 +1,17 @@
 
 
 function Services() {
-  return (
-    <div>Services</div>
-  )
+    return (
+        <>
+            <h1>Services</h1>
+            <ul>
+                <li><strong>Web Development:</strong> Building responsive, scalable, and accessible websites using the MERN stack.</li>
+                <li><strong>Front-End Design:</strong> Crafting intuitive user interfaces with React.js, HTML, CSS, and Bootstrap.</li>
+                <li><strong>Full-Stack Solutions:</strong> Developing end-to-end applications with Node.js, Express, and MongoDB.</li>
+                <li><strong>Responsive & Accessible Design:</strong> Ensuring websites are optimized for all devices and accessible to all users.</li>
+            </ul>
+        </>
+    )
 }
 
 export default Services

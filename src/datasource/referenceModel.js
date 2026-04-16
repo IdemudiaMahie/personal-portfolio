9b5552521c6e29263ab3e7563625e4ac8faf6194
IdemudiaMahie <idemudiamahie23@gmail.com>
2026-04-16 17:58:42 -0400

    Created referenceModel.js

---
+++ src/datasource/referenceModel.js
@@ -0,0 +1,10 @@
+class ReferenceModel {
+    constructor(id = "", name = "", company = "", message = "") {
+        this.id = id;
+        this.name = name;
+        this.company = company;
+        this.message = message;
+    }
+}
+
+export default ReferenceModel;
